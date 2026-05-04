d900c09ad0b3d5bb712b5515a56e463a7c626082
Leonardo Julian Amarilla Almada <leonardo.amarilla@softtek.com>
2026-05-04 12:04:23 +0200

    Creado data.sql para datos para pruebas, properties cambiado para el uso de base de datos

---
+++ ejercicioLogging/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (id, name) VALUES ('USR001', 'Francisco Sánchez Gómez');
+INSERT INTO users (id, name) VALUES ('USR002', 'Ana López Martín');
+INSERT INTO users (id, name) VALUES ('USR003', 'Carlos Ruiz Pérez');
+
+-- Apuestas de USR001
+INSERT INTO bets (id, user_id) VALUES (1, 'USR001');
+INSERT INTO bet_numbers (bet_id, position, number) VALUES
+                                                       (1, 0, 23), (1, 1, 45), (1, 2, 2), (1, 3, 10), (1, 4, 11), (1, 5, 43);
+
+INSERT INTO bets (id, user_id) VALUES (2, 'USR001');
+INSERT INTO bet_numbers (bet_id, position, number) VALUES
+                                                       (2, 0, 34), (2, 1, 21), (2, 2, 7), (2, 3, 3), (2, 4, 9), (2, 5, 8);
+
+-- Apuesta de USR002
+INSERT INTO bets (id, user_id) VALUES (3, 'USR002');
+INSERT INTO bet_numbers (bet_id, position, number) VALUES
+                                                       (3, 0, 1), (3, 1, 13), (3, 2, 17), (3, 3, 25), (3, 4, 36), (3, 5, 49);
