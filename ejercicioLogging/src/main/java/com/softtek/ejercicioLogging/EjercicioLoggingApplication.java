16738880f77e9b0df87832b09cc2aa216b81cc47
Jaime-Softtek <jaime.castillo@softtek.com>
2026-05-04 11:36:18 +0200

    Add files via upload
    UploadProject

---
+++ ejercicioLogging/src/main/java/com/softtek/ejercicioLogging/EjercicioLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.softtek.ejercicioLogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioLoggingApplication.class, args);
+	}
+
+}
